bdc9ad3367174d84058a580749c1fbbcc4676790
Treimart <treier@tlu.ee>
2023-09-15 18:14:21 +0300

    Rewrote the basics.js file with new examples to the Homework folder

---
+++ Homework/21-09-2023/additional.js
@@ -0,0 +1,42 @@
+// Difference between == and ===
+
+console.log(101 == "101")
+console.log(101 === "101")
+
+// async/await
+
+/*
+Async'i ja await'i kasutatakse siis, kui enne koodi (funktsiooni)
+algatamist on vaja mingi protsess (või ajavahemik) ära oodata.
+
+async - märksõna, mida kasutatakse funktsiooni liigi määramiseks,
+milles on await sees.
+await - märksõna, mis on kasutusel, et käivitada mingi osa koodist
+peale seda, kui async'i osa on tehtud.
+*/
+
+function delay(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+async function delayedNotification() {
+    console.log('Keskmise inimese tähelepanu kestvus on alla kahe sekundi');
+    await delay(2000);
+    console.log('Tundub, et teie olete üle keskmise.');
+}
+
+delayedNotification();
+
+// Promise
+/*
+Promise on JS'is objekt, mis tähistab mingisuguse asünkroonse
+protsessi läbiviimist (või läbikukkumist).
+Seda kasutades on võimalik edasi viia Promise'le järgnevaid
+asünkroonseid protsesse ilma järgnevat koodi blokeerides.
+
+Ülaltoodud koodis on Promise'it kasutatud timeout'i määramiseks.
+Peale timeout'i määramist on koodil võimalik koheselt edasi minna.
+
+Promise't kasutatakse ka failidest, API'st või andmebaasidest
+igasugusel informatsioonitöötlusel.
+*/
